2ac0bd86225a8d0fdb0d5b235562f0c3cdbb595f
윤성현 <41726251+YOONSUNGHYEON@users.noreply.github.com>
2021-10-25 17:07:00 +0900

    Feat : 기준상품등록 페이지 추가

---
+++ src/main/java/office/DanawaOfficeProjectApplication.java
@@ -0,0 +1,13 @@
+package office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanawaOfficeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanawaOfficeProjectApplication.class, args);
+	}
+
+}
